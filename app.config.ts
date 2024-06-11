00bd0eebf9369d3346bf568f9cb3fd6f0de67024
leonardoelit <leonardoelitgold@gmail.com>
2024-06-11 14:43:13 +0300

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
-  title: 'Sink',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  title: 'Elit Bet Lisanslı Bahis Sitesi',
+  description: 'Lisanslı, Güvenilir Bahis ve Casino Sitesi Elit Bet ile Oyun Keyfinin Tadını Çıkar ',
   image: 'https://sink.cool/banner.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
